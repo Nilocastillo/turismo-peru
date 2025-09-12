2ff8cdeb6a3a596edf4cb4ee66cf8815bda25e9a
Nilo <nilocastillloumeres@gmail.com>
2025-09-11 20:09:15 -0500

    imagenes de cada tours y optimización

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
-export const tours = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    duration: z.string(),
-    location: z.string(),
-    departure: z.string(),
-    category: z.string(),
-    description: z.string(),
-    image: z.string(),
-    alt: z.string(), // 👈 obligatorio
-  }),
+const tours = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      duration: z.string(),
+      location: z.string(),
+      departure: z.string(),
+      category: z.string(),
+      description: z.string(),
+      image: image(),
+      alt: z.string(),
+    }),
 });
 
 export const collections = { tours };
