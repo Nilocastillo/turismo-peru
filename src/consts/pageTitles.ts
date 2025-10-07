a151a9933ac028771c5c9c04ff92d7645b9ef8a2
Nilo <nilocastillloumeres@gmail.com>
2025-10-06 19:13:23 -0500

    seo y paginas optimizadas

---
+++ src/consts/pageTitles.ts
@@ -0,0 +1,4 @@
+export const fixedTitle: string = `Turismo Perú Consultores - Agencia de Viajes en Perú y Machu Picchu`;
+export const aventura: string = `Tours de aventura - ${fixedTitle}`;
+export const cultural: string = `Tours culturales - ${fixedTitle}`;
+export const lujo: string = `Tours de lujo - ${fixedTitle}`;
